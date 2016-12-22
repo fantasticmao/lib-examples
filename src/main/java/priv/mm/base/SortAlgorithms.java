58dc7c8e365018b12b3de013899d56bcd5099e45
fantasticmao <maomao8017@gmail.com>
2016-12-22 16:47:24 +0800

    更新SortAlgorithms： 1. 参照wiki上示例，更新希尔排序的写法（似乎可读性变差了...）。 2.  使用更合理的变量名

--- src/main/java/priv/mm/base/SortAlgorithms.java
+++ src/main/java/priv/mm/base/SortAlgorithms.java
@@ -16,19 +16,20 @@ public class SortAlgorithms {
      * 2. 如果前者元素大于后者，则交换它们位置。
      * 3. 向后移动一位，重复1-2。
      */
-    private static int[] bubbleSort(int[] A) {
-        int[] a = Arrays.copyOf(A, A.length);
+    private static int[] bubbleSort(int[] a) {
+        int len = a.length;
+        int[] arr = Arrays.copyOf(a, len);
         int temp;
-        for (int i = a.length - 1; i > 0; i--) {
+        for (int i = len - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (a[j] > a[j + 1]) {
-                    temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-        return a;
+        return arr;
     }
 
     /**
@@ -37,83 +38,84 @@ public class SortAlgorithms {
      * 2. 若min!=i，则交换最小元素a[min]与初始元素a[i]。
      * 3. i++，重复1-2步骤。
      */
-    private static int[] selectSort(int[] A) {
-        int[] a = Arrays.copyOf(A, A.length);
+    private static int[] selectSort(int[] a) {
+        int len = a.length;
+        int[] arr = Arrays.copyOf(a, len);
         int temp;
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < len - 1; i++) {
             int min = i;
-            for (int j = i; j < a.length - 1; j++) {
-                if (a[min] > a[j + 1]) {
+            for (int j = i; j < len - 1; j++) {
+                if (arr[min] > arr[j + 1]) {
                     min = j + 1;
                 }
             }
             if (min != i) {
-                temp = a[i];
-                a[i] = a[min];
-                a[min] = temp;
+                temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
             }
         }
-        return a;
+        return arr;
     }
 
     /**
      * 插入排序
      * 把无序区的第一个元素key插入到有序区的合适位置。
      */
-    private static int[] insertSort(int[] A) {
-        int[] a = Arrays.copyOf(A, A.length);
-        for (int i = 1; i < a.length; i++) {
-            int key = a[i];
+    private static int[] insertSort(int[] a) {
+        int len = a.length;
+        int[] arr = Arrays.copyOf(a, len);
+        int temp;
+        for (int i = 1; i < len; i++) {
+            temp = arr[i];
             int j = i - 1;
-            while (j >= 0 && key < a[j]) {
-                a[j + 1] = a[j];
-                j--;
+            for (; j >= 0 && temp < arr[j]; j--) {
+                arr[j + 1] = arr[j];
             }
-            a[j + 1] = key;
+            arr[j + 1] = temp;
         }
-        return a;
+        return arr;
     }
-
 
     /**
      * 希尔（缩小增量）排序
      */
-    private static int[] shellSort(int[] A) {
-        int[] a = Arrays.copyOf(A, A.length);
-        int h = 1;// h=1, 4, 13, 40, 121...
-        while (h < a.length)
-            h = h * 3 + 1;
-        h = --h / 3;
-        for (; h > 0; h = --h / 3) {
-            for (int i = h; i < a.length; i++) {
-                int key = a[i];
-                int j = i - h;
-                while (j >= 0 && key < a[j]) {
-                    a[j + h] = a[j];
-                    j -= h;
+    private static int[] shellSort(int[] a) {
+        int len = a.length;
+        int[] arr = Arrays.copyOf(a, len);
+        int h = 1;
+        while (h < len / 3) {
+            h = h * 3 + 1; // Knuth序列 h = 1, 4, 13, 40, 121...
+        }
+        for (int i, j, temp; h > 0; h /= 3) {
+            for (i = h; i < len; i++) {
+                temp = arr[i];
+                for (j = i - h; j >= 0 && temp < arr[j]; j -= h) {
+                    arr[j + h] = arr[j];
                 }
-                a[j + h] = key;
+                arr[j + h] = temp;
             }
         }
-        return a;
+        return arr;
     }
 
-    private static int[] quickSort(int[] A) {
-        int[] a = Arrays.copyOf(A, A.length);
-        return a;
+    private static int[] quickSort(int[] a) {
+        int len = a.length;
+        int[] arr = Arrays.copyOf(a, len);
+        return arr;
     }
 
     public static void main(String[] args) {
         Random random = new Random();
-        int[] A = new int[10];
-        for (int i = 0; i < A.length; i++) {
-            A[i] = random.nextInt(100);
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(100);
         }
-        //System.out.println(Arrays.toString(A));
-        //System.out.println("bubbleSort: " + Arrays.toString(bubbleSort(A)));
-        //System.out.println("selectSort: " + Arrays.toString(selectSort(A)));
-        //System.out.println("insertSort: " + Arrays.toString(insertSort(A)));
-        System.out.println("shellSort: " + Arrays.toString(shellSort(A)));
-        //System.out.println("quickSort: " + Arrays.toString(quickSort(A)));
+        System.out.println(Arrays.toString(a));
+        System.out.println("bubbleSort: " + Arrays.toString(bubbleSort(a)));
+        System.out.println("selectSort: " + Arrays.toString(selectSort(a)));
+        System.out.println("insertSort: " + Arrays.toString(insertSort(a)));
+        System.out.println("shellSort: " + Arrays.toString(shellSort(a)));
+        //System.out.println("quickSort: " + Arrays.toString(quickSort(a)));
     }
 }
