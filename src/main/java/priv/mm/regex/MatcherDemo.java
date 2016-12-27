5fc96714ec2ba366d45c61fb27720e7e90c2e7d5
FantasticMao <maodh@dxy.cn>
2016-12-27 19:21:09 +0800

    研究 泛型

---
+++ src/main/java/priv/mm/regex/MatcherDemo.java
@@ -0,0 +1,39 @@
+package priv.mm.regex;
+
+/**
+ * MatcherDemo
+ * Created by maomao on 16-12-23.
+ */
+public class MatcherDemo {
+    private static void matches() {
+
+    }
+
+    private static void lookingAt() {
+
+    }
+
+    private static void find() {
+
+    }
+
+    private static void group() {
+
+    }
+
+    private static void split() {
+
+    }
+
+    private static void replace() {
+
+    }
+
+    private static void reset() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
