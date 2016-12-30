38e74cfa21407ae4e5c1d00193331c9233f18f09
maomao <maomao8017@gmail.com>
2016-12-30 17:27:00 +0800

    了解 类加载机制

---
+++ src/main/java/priv/mm/reference/ClassLoaderDemo.java
@@ -0,0 +1,39 @@
+package priv.mm.reference;
+
+/**
+ * 类加载Demo
+ * <p>
+ * 所有的类都是在对其第一次使用时，动态加载到JVM，构造器算类的静态方法。
+ * static语句在类第一次被加载时执行
+ */
+class A {
+    static {
+        System.out.println("A load ...");
+    }
+}
+
+class B {
+    static {
+        System.out.println("B load ...");
+    }
+}
+
+class C {
+    static {
+        System.out.println("C load ...");
+    }
+}
+
+public class ClassLoaderDemo {
+
+    public static void main(String[] args) {
+        try {
+            //
+            Class.forName("priv.mm.reference.B");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        new B();
+        new C();
+    }
+}
