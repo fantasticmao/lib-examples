fc2215aaa063bcb377d35c5549874412aedbced3
maomao <maomao8017@gmail.com>
2017-02-10 18:05:48 +0800

    更新 Stream和Date/Time

--- src/main/java/priv/mm/java8/StreamAPIDemo.java
+++ src/main/java/priv/mm/java8/StreamAPIDemo.java
@@ -1,6 +1,8 @@
 package priv.mm.java8;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * StreamAPI
@@ -12,17 +14,28 @@ import java.util.stream.Stream;
  */
 public class StreamAPIDemo {
     public static void main(String[] args) {
-        Stream.of("one", "two", "three")
-                .forEach(e -> System.out.print(e + " "));
-        System.out.println();
-
-        Stream.of(1, 2, 3)
-                .map((t) -> ++t)
-                .forEach(e -> System.out.print(e + " "));
-        System.out.println();
-
-        Stream.of(1, 1, 2, 2, 2, 3)
+        List<Integer> list1 = Arrays.asList(1, 1, null, 2, 3, 4, null, 5, 8, 7, 6, 9);
+        long count = list1.stream()
+                .filter(Objects::nonNull)
                 .distinct()
-                .forEach(e -> System.out.print(e + " "));
+                .sorted()
+                .mapToInt(e -> e)
+                .peek(System.out::println)
+                .skip(2)
+                .limit(3)
+                .count();
+        System.out.println(count);
+
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        Integer sum = list2.stream().reduce(0, (x, y) -> x + y);
+        System.out.println(sum);
+
+        // min返回的是一个Optional对象
+        Integer min = list2.stream().min(Integer::compareTo).orElse(0);
+        System.out.println(min);
+
+        Boolean isMatch = list2.stream().anyMatch(e -> e == 3);
+        System.out.println(isMatch);
     }
 }
