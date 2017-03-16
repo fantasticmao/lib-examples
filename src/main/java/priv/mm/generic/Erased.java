e5e7e66a9e7c64edf73b4e2c89048e5e05ac48b3
FantasticMao <maomao8017@gmail.com>
2017-03-16 22:57:10 +0800

    更新 Java 8 Date/Time

---
+++ src/main/java/priv/mm/generic/Erased.java
@@ -0,0 +1,14 @@
+package priv.mm.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Erased<T> {
+    private T t;
+
+    public void foo(Class<T> clazz) throws Exception {
+        boolean f = t instanceof Integer; // ok
+        this.t = clazz.newInstance(); // error
+        List<T> array1 = new ArrayList<>(); // error
+    }
+}
