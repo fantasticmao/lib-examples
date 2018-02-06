052332fbdce7ebc77610afa4f8fa4098fd39e2ec
FantasticMao <maomao8017@gmail.com>
2018-02-07 01:03:35 +0800

    new file:   src/main/java/priv/mm/io/FilesDemo.java

--- src/main/java/priv/mm/io/RandomAccessFileDemo.java
+++ src/main/java/priv/mm/io/RandomAccessFileDemo.java
@@ -17,7 +17,8 @@ public class RandomAccessFileDemo {
         final int SIZE = 3; // UTF-8 编码中，常用汉字占 3 字节
         final byte[] bytes = new byte[12 * SIZE]; // 读取 12 * SIZE 字节
 
-        File file = new File("/Users/maomao/IdeaProjects/Demo/src/main/java/priv/mm/io/声声慢.txt");
+        String classPath = RandomAccessFileDemo.class.getResource("/").getPath();
+        File file = new File(classPath + "/声声慢.txt");
         try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
             final long length = randomAccessFile.length();
             randomAccessFile.seek(length - 12 * SIZE);
