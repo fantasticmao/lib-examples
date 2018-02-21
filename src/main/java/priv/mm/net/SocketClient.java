58d71b9b6907988cabe0b3de975a430ac4e2c06f
FantasticMao <maomao8017@gmail.com>
2018-02-21 15:19:11 +0800

    modified:   src/main/java/priv/mm/net/SocketClient.java

--- src/main/java/priv/mm/net/SocketClient.java
+++ src/main/java/priv/mm/net/SocketClient.java
@@ -1,11 +1,14 @@
 package priv.mm.net;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * SocketClient
+ * 服务器发送给服务器输出流的所有信息都会成为客户端程序的输入，
+ * 同时来自客户端程序的所有输出都会被包含在服务器输入流中。
  *
  * @author maodh
  * @since 2017/9/4
@@ -13,10 +16,11 @@ import java.net.Socket;
 public class SocketClient {
 
     public static void main(String[] args) throws IOException {
-        Socket client = new Socket("localhost", 8080);
-        PrintStream out = new PrintStream(client.getOutputStream());
-        out.print("hello world!");
-        out.close();
-        client.close();
+        try (Socket client = new Socket("localhost", 9999);
+             Scanner scanner = new Scanner(client.getInputStream(), StandardCharsets.UTF_8.name())) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        }
     }
 }
