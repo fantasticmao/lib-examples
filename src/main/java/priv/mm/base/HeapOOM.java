6d492fdad6161b380a3ca0883cdaa487b52a599e
maodh <maodh@dxy.cn>
2018-07-05 14:39:49 +0800

    补充 JVM GC 参数

--- src/main/java/priv/mm/base/HeapOOM.java
+++ src/main/java/priv/mm/base/HeapOOM.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 /**
  * HeapOOM
- * -Xms10m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError
+ * -Xms20m -Xmx20m -XX:+UseParallelGC -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
+ * -XX:+UseSerialGC 虚拟机在 Client 模式下的默认值，使用 Serial + Serial Old 的收集器组合进行内存回收
+ * -XX:+UseParNewGC 使用 ParNew + Serial Old 的收集器组合进行内存回收
+ * -XX:+UseConcMarkSweepGC 使用 ParNew + CMS + Serial Old 的收集器组合进行内存回收，Serial Old 收集器将作为 CMS 收集器出现 Concurrent Mode Failure 失败后的后备收集器使用
+ * -XX:+UseParallelGC 虚拟机在 Server 模式下的默认值，使用 Parallel Scavenge + Parallel Old 的收集器组合进行内存回收
  *
  * @author maodh
  * @since 22/05/2018
