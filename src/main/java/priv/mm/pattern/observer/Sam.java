99e3da39ec1ea3a22ce0176baee48d4d633a3e5e
fantasticmao <maomao8017@gmail.com>
2018-07-16 02:40:16 +0800

    更新 观察者模式

---
+++ src/main/java/priv/mm/pattern/observer/Sam.java
@@ -0,0 +1,29 @@
+package priv.mm.pattern.observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Sam
+ *
+ * @author maodh
+ * @since 2018/7/16
+ */
+public class Sam implements Observer {
+    private Message message;
+
+    public Sam(Message message) {
+        this.message = message;
+        this.message.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println(this.toString() + " get a message: \"" + arg.toString() + "\" from: " + o.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Sam";
+    }
+}
