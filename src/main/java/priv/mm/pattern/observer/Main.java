08b4d1258c48b8e2df4a00f26019193ded450f53
maodh <maodh@dxy.cn>
2018-07-16 02:40:16 +0800

    更新 观察者模式

---
+++ src/main/java/priv/mm/pattern/observer/Main.java
@@ -0,0 +1,17 @@
+package priv.mm.pattern.observer;
+
+/**
+ * Main
+ *
+ * @author maodh
+ * @since 2018/7/16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Message message = new Message();
+        Tom tom = new Tom(message);
+        Sam sam = new Sam(message);
+        message.sendMessage("Good Evening");
+    }
+}
