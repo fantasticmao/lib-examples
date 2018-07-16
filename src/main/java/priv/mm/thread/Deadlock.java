d163792ae95e6bde62474a3ae4a87aa742f9f533
maodh <maodh@dxy.cn>
2018-07-16 13:25:38 +0800

    modified:   src/main/java/priv/mm/thread/Deadlock.java

--- src/main/java/priv/mm/thread/Deadlock.java
+++ src/main/java/priv/mm/thread/Deadlock.java
@@ -1,5 +1,7 @@
 package priv.mm.thread;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Deadlock
  *
@@ -7,39 +9,62 @@ package priv.mm.thread;
  * @since 2018/7/5
  */
 public class Deadlock {
-    public static Object lock = new Object();
 
     private static class Run1 implements Runnable {
+        private Object lock1;
+        private Object lock2;
+
+        public Run1(Object lock1, Object lock2) {
+            this.lock1 = lock1;
+            this.lock2 = lock2;
+        }
 
         @Override
         public void run() {
-            synchronized (lock) {
+            // 持有 lock1，等待 lock2
+            synchronized (lock1) {
                 try {
-                    lock.wait();
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }
+                synchronized (lock2) {
+
                 }
             }
         }
     }
 
     private static class Run2 implements Runnable {
+        private Object lock1;
+        private Object lock2;
+
+        public Run2(Object lock1, Object lock2) {
+            this.lock1 = lock1;
+            this.lock2 = lock2;
+        }
 
         @Override
         public void run() {
-            synchronized (lock) {
+            // 持有 lock2，等待 lock1
+            synchronized (lock2) {
                 try {
-                    lock.wait();
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                }
+                synchronized (lock1) {
+
                 }
             }
         }
     }
 
     public static void main(String[] args) {
-        Run1 run1 = new Run1();
-        Run2 run2 = new Run2();
+        String lock1 = "lock1";
+        String lock2 = "lock2";
+        Run1 run1 = new Run1(lock1, lock2);
+        Run2 run2 = new Run2(lock1, lock2);
 
         new Thread(run1, "run1").start();
         new Thread(run2, "run2").start();
