a6288fbc81a8fd54e297d5a3120a3db97befbf9c
maodh <maodh@dxy.cn>
2018-07-16 22:37:24 +0800

    modified:   src/main/java/priv/mm/thread/Deadlock.java

--- src/main/java/priv/mm/thread/Deadlock.java
+++ src/main/java/priv/mm/thread/Deadlock.java
@@ -10,18 +10,10 @@ import java.util.concurrent.TimeUnit;
  */
 public class Deadlock {
 
-    private static class Run1 implements Runnable {
-        private Object lock1;
-        private Object lock2;
-
-        public Run1(Object lock1, Object lock2) {
-            this.lock1 = lock1;
-            this.lock2 = lock2;
-        }
-
-        @Override
-        public void run() {
-            // 持有 lock1，等待 lock2
+    public static void main(String[] args) {
+        Object lock1 = new Object();
+        Object lock2 = new Object();
+        new Thread(() -> {// 持有 lock1，等待 lock2
             synchronized (lock1) {
                 try {
                     TimeUnit.SECONDS.sleep(1);
@@ -32,21 +24,9 @@ public class Deadlock {
 
                 }
             }
-        }
-    }
+        }, "run1").start();
 
-    private static class Run2 implements Runnable {
-        private Object lock1;
-        private Object lock2;
-
-        public Run2(Object lock1, Object lock2) {
-            this.lock1 = lock1;
-            this.lock2 = lock2;
-        }
-
-        @Override
-        public void run() {
-            // 持有 lock2，等待 lock1
+        new Thread(() -> {// 持有 lock2，等待 lock1
             synchronized (lock2) {
                 try {
                     TimeUnit.SECONDS.sleep(1);
@@ -57,16 +37,6 @@ public class Deadlock {
 
                 }
             }
-        }
-    }
-
-    public static void main(String[] args) {
-        String lock1 = "lock1";
-        String lock2 = "lock2";
-        Run1 run1 = new Run1(lock1, lock2);
-        Run2 run2 = new Run2(lock1, lock2);
-
-        new Thread(run1, "run1").start();
-        new Thread(run2, "run2").start();
+        }, "run2").start();
     }
 }
