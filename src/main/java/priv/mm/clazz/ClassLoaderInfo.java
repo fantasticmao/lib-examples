fc4e516f8eecd091f54dadb1afb23f9c89661c5b
FantasticMao <maomao8017@gmail.com>
2018-07-19 03:01:59 +0800

    new file:   src/main/java/priv/mm/thread/ThreadContextClassLoaderDemo.java

--- src/main/java/priv/mm/clazz/ClassLoaderInfo.java
+++ src/main/java/priv/mm/clazz/ClassLoaderInfo.java
@@ -24,6 +24,8 @@ public class ClassLoaderInfo {
     /**
      * {@code ExtClassloader} 扩展类加载器，又称应用加载器，负责加载 Java 的扩展类库，默认加载 JAVA_HOME/jre/lib/ext 目录下所有 jar 包，
      * 或者由 java.ext.dirs 系统指定的 jar 包。
+     *
+     * @see sun.misc.Launcher.ExtClassLoader
      */
     private static void extClassLoader() {
         String extDirs = System.getProperty("java.ext.dirs");
@@ -37,6 +39,8 @@ public class ClassLoaderInfo {
      * {@code AppClassloader} 系统类加载器，又称应用加载器，负责在 JVM 启动时加载来自 Java 中的 -classpath 或 java.class.path 系统属性，
      * 或者 CLASSPATH 操作系统属性所指定的 JAR 类包路径。调用 {@link ClassLoader#getSystemClassLoader()} 可以获取该类加载器。
      * 如果没有特别指定，用户自定义的任何类加载器都应将 {@link ClassLoader} 作为它的父类加载器。
+     *
+     * @see sun.misc.Launcher.AppClassLoader
      */
     private static void appClassLoader() {
         String path = System.getProperty("java.class.path");
