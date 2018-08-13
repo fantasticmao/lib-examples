a9a18937c795c04736cc316fa36f32dcaa5cdd40
FantasticMao <maomao8017@gmail.com>
2018-08-13 17:39:57 +0800

    更新 Process

---
+++ src/main/java/priv/mm/base/ProcessDemo.java
@@ -0,0 +1,21 @@
+package priv.mm.base;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * ProcessDemo
+ *
+ * @author maodh
+ * @since 2018/8/13
+ */
+public class ProcessDemo {
+
+    public static void main(String[] args) throws IOException {
+        Process process = Runtime.getRuntime().exec("date");
+        InputStream inputStream = process.getInputStream();
+        byte[] bytes = new byte[1 << 5];
+        inputStream.read(bytes);
+        System.out.println(new String(bytes));
+    }
+}
