a9287bb3b6af1f4e04a63bb207ef77e2a8ec0754
FantasticMao <maomao8017@gmail.com>
2018-10-24 22:05:12 +0800

    new file
    modified:   pom.xml
    new file:   src/main/java/priv/mm/grpc/HelloWorldClient.java
    new file:   src/main/java/priv/mm/grpc/HelloWorldServer.java
    new file:   src/main/java/priv/mm/netty/discard/DiscardServer.java
    new file:   src/main/java/priv/mm/netty/discard/DiscardServerHandler.java
    new file:   src/main/java/priv/mm/netty/time/TimeClient.java
    new file:   src/main/java/priv/mm/netty/time/TimeClientHandler.java
    new file:   src/main/java/priv/mm/netty/time/TimeServerHandler.java
    new file:   src/main/proto/helloworld.proto

---
+++ src/main/java/priv/mm/netty/time/TimeClientHandler.java
@@ -0,0 +1,34 @@
+package priv.mm.netty.time;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+/**
+ * TimeClientHandler
+ *
+ * @author maodh
+ * @since 2018/6/25
+ */
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf m = (ByteBuf) msg;
+        try {
+            long currentTimeMillis = m.readUnsignedInt();
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
