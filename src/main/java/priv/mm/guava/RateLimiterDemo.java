4269990aa47b69b0cf4ea82c159800bb20ecbb3d
FantasticMao <maomao8017@gmail.com>
2018-12-10 14:22:12 +0800

    new file
    modified:   pom.xml
    renamed:    src/main/java/priv/mm/data_structure_and_algorithm/BloomFilterDemo.java -> src/main/java/priv/mm/guava/BloomFilterDemo.java
    new file:   src/main/java/priv/mm/guava/RateLimiterDemo.java

---
+++ src/main/java/priv/mm/guava/RateLimiterDemo.java
@@ -0,0 +1,31 @@
+package priv.mm.guava;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * RateLimiterDemo
+ *
+ * @author maodh
+ * @since 2018/12/10
+ */
+public class RateLimiterDemo {
+
+    public static void main(String[] args) {
+        // 1. 限制每秒钟两个请求
+        final int permitsPerSecond = 3;
+        RateLimiter limiter = RateLimiter.create(permitsPerSecond);
+
+        // 2. 受限制地执行任务
+        final int size = 10;
+        final long start = System.nanoTime();
+        for (int i = 0; i < size; i++) {
+            limiter.acquire();
+            System.out.println("Hello " + i + ", are you OK?");
+        }
+        final long end = System.nanoTime();
+
+        // 3. 计算执行时间
+        // 计算结果应为  (size - 1) / permitsPerSecond
+        System.out.println((end - start) / Math.pow(10, 9));
+    }
+}
