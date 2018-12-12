5b66914b36ff3f8557b8593ef71fd8a454c4a5bb
FantasticMao <maomao8017@gmail.com>
2018-12-12 15:26:59 +0800

    new file
    modified:   pom.xml
    new file:   src/main/java/priv/mm/memcached/MemcachedDemo.java

---
+++ src/main/java/priv/mm/memcached/MemcachedDemo.java
@@ -0,0 +1,26 @@
+package priv.mm.memcached;
+
+import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.impl.ArrayMemcachedSessionLocator;
+import net.rubyeye.xmemcached.impl.KetamaMemcachedSessionLocator;
+
+/**
+ * MemcachedDemo
+ *
+ * @author maodh
+ * @since 2018/12/11
+ */
+public class MemcachedDemo {
+
+    public static void main(String[] args) throws Exception {
+        MemcachedClientBuilder build = new XMemcachedClientBuilder("localhost:11211 localhost:22422");
+        //build.setSessionLocator(new ArrayMemcachedSessionLocator());
+        build.setSessionLocator(new KetamaMemcachedSessionLocator());
+        MemcachedClient memcachedClient = build.build();
+        final String key = "key";
+        final String value = memcachedClient.get(key);
+        System.out.println(value);
+    }
+}
