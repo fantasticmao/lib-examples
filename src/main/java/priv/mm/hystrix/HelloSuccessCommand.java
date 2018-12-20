c9ac2dc3ef46aa8f50a6d4e07d8652825d717ba6
FantasticMao <maomao8017@gmail.com>
2018-12-20 22:53:55 +0800

    new file
    modified:   pom.xml
    new file:   src/main/java/priv/mm/hystrix/HelloFailureCommand.java
    new file:   src/main/java/priv/mm/hystrix/HelloSuccessCommand.java

---
+++ src/main/java/priv/mm/hystrix/HelloSuccessCommand.java
@@ -0,0 +1,41 @@
+package priv.mm.hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * HelloSuccessCommand
+ *
+ * @author maodh
+ * @since 2018/12/20
+ */
+public class HelloSuccessCommand extends HystrixCommand<String> {
+    private String name;
+
+    public HelloSuccessCommand(String name) {
+        super(HystrixCommandGroupKey.Factory.asKey("hello"));
+        this.name = name;
+    }
+
+    @Override
+    protected String run() throws Exception {
+        return "Hello " + name + "!";
+    }
+
+    @Override
+    protected String getFallback() {
+        return "Hello Failure " + name + "!";
+    }
+
+    public static class UnitTest {
+
+        @Test
+        public void test() {
+            String name = "MaoMao";
+            HelloSuccessCommand command = new HelloSuccessCommand("MaoMao");
+            Assert.assertEquals("Hello " + name + "!", command.execute());
+        }
+    }
+}
