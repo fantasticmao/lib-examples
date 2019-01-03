c954a3a94659fa929501173d5a458b7e14960a97
FantasticMao <maomao8017@gmail.com>
2019-01-03 13:15:52 +0800

    new file
    new file:   src/main/java/priv/mm/concurrent/simulation/TimedRun.java

---
+++ src/main/java/priv/mm/concurrent/simulation/TimedRun.java
@@ -0,0 +1,59 @@
+package priv.mm.concurrent.simulation;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * TimedRun
+ *
+ * @author maodh
+ * @since 2019/1/3
+ */
+public class TimedRun {
+
+    private void run1(int second, Runnable runnable) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                runnable.run();
+            }
+        });
+        thread.start();
+        TimeUnit.SECONDS.sleep(second);
+        thread.interrupt();
+    }
+
+    private void run2(int second, Runnable runnable) throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            while (!Thread.interrupted()) {
+                runnable.run();
+            }
+        });
+        TimeUnit.SECONDS.sleep(second);
+        executorService.shutdownNow();
+    }
+
+    private void run3(int second, Runnable runnable) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future future = executorService.submit(() -> {
+            while (!Thread.interrupted()) {
+                runnable.run();
+            }
+        });
+        try {
+            future.get(second, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            // ignore
+        } finally {
+            future.cancel(true);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TimedRun timedRun = new TimedRun();
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+        timedRun.run1(1, () -> System.out.println(atomicInteger.getAndIncrement()));
+    }
+}
