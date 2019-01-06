9622483dd780a2917771feed1e98bf26d5d1dc99
FantasticMao <maomao8017@gmail.com>
2019-01-06 23:54:08 +0800

    modified
    modified:   src/main/java/priv/mm/concurrent/ThreadLocalDemo.java

--- src/main/java/priv/mm/concurrent/ThreadLocalDemo.java
+++ src/main/java/priv/mm/concurrent/ThreadLocalDemo.java
@@ -10,16 +10,21 @@ import java.util.concurrent.Executors;
  * @since 2018/7/26
  */
 public class ThreadLocalDemo {
-    private static final ThreadLocal<Integer> THREAD_LOCAL_INTEGER = ThreadLocal.withInitial(() -> 1);
+    private static final ThreadLocal<Object> THREAD_LOCAL_INTEGER = ThreadLocal.withInitial(Object::new);
 
     public static void main(String[] args) {
         int poolSize = 5;
         ExecutorService executorService = Executors.newFixedThreadPool(poolSize);
         for (int i = 0; i < poolSize; i++) {
             executorService.execute(() -> {
-                System.out.println(String.format("ThreadName: %s ThreadLocal<Integer>: %d\n",
+                System.out.println(String.format("ThreadName: %s ThreadLocal<Object> hashCode: %d\n",
                         Thread.currentThread().getName(),
-                        ThreadLocalDemo.THREAD_LOCAL_INTEGER.get()));
+                        ThreadLocalDemo.THREAD_LOCAL_INTEGER.get().hashCode()));
+            });
+            executorService.execute(() -> {
+                System.out.println(String.format("ThreadName: %s ThreadLocal<Object> hashCode: %d\n",
+                        Thread.currentThread().getName(),
+                        ThreadLocalDemo.THREAD_LOCAL_INTEGER.get().hashCode()));
             });
         }
         executorService.shutdown();
