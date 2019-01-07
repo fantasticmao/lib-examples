809cd26b92a43c5c76903c6bf8c5e3d109c299a0
FantasticMao <maomao8017@gmail.com>
2019-01-07 17:44:25 +0800

    添加 NIO/BIO 示例代码
    new file:   src/main/java/priv/mm/io/net/SocketBioClient.java
    new file:   src/main/java/priv/mm/io/net/SocketBioServerUseExecutor.java
    new file:   src/main/java/priv/mm/io/net/SocketBioServerUseMultipleThread.java
    new file:   src/main/java/priv/mm/io/net/SocketBioServerUseSingleThread.java
    deleted:    src/main/java/priv/mm/io/net/SocketClient.java
    new file:   src/main/java/priv/mm/io/net/SocketNioClient.java
    new file:   src/main/java/priv/mm/io/net/SocketNioServer.java
    deleted:    src/main/java/priv/mm/io/net/SocketSelectorServer.java
    deleted:    src/main/java/priv/mm/io/net/SocketTest.java
    deleted:    src/main/java/priv/mm/io/net/SocketThreadServer.java

---
+++ src/main/java/priv/mm/io/net/SocketBioServerUseMultipleThread.java
@@ -0,0 +1,62 @@
+package priv.mm.io.net;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * SocketBioServerUseMultipleThread
+ *
+ * @author maodh
+ * @see <a href="https://javadoop.com/post/java-nio">Java NIO：Buffer、Channel 和 Selector </a>
+ * @see <a href="https://javadoop.com/post/nio-and-aio">Java 非阻塞 IO 和异步 IO</a>
+ * @see <a href="https://www.ibm.com/developerworks/java/library/j-javaio/">多路复用 IO</a>
+ * @see <a href="https://www.ibm.com/developerworks/cn/java/j-lo-io-optimize/index.html">概念：同步阻塞IO、同步非阻塞IO、异步非阻塞IO</a>
+ * @since 2017/9/4
+ */
+public class SocketBioServerUseMultipleThread {
+
+    public static void main(String[] args) throws IOException {
+        final int port = 9999;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept(); // 线程处于阻塞状态，等待接收连接
+                // 此处可以使用线程池方式优化
+                Runnable handler = new ThreadEchoHandler(socket);
+                new Thread(handler).start();
+            }
+        }
+    }
+
+    public static class ThreadEchoHandler implements Runnable, Closeable {
+        private Socket socket;
+
+        public ThreadEchoHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void close() {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try (OutputStream out = socket.getOutputStream();
+                 PrintWriter writer = new PrintWriter(out)) {
+                writer.print("hello client");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                this.close();
+            }
+        }
+    }
+}
---
+++ src/main/java/priv/mm/io/net/SocketBioServerUseSingleThread.java
@@ -0,0 +1,29 @@
+package priv.mm.io.net;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * SocketBioServerUseSingleThread
+ *
+ * @author maodh
+ * @since 2019/1/7
+ */
+public class SocketBioServerUseSingleThread {
+
+    public static void main(String[] args) throws IOException {
+        final int port = 9999;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept(); // 线程处于阻塞状态，等待接收连接
+                try (OutputStream out = socket.getOutputStream();
+                     PrintWriter writer = new PrintWriter(out)) {
+                    writer.print("hello client");
+                }
+            }
+        }
+    }
+}
