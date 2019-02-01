45ba72c6fcdd0e1a79fabaf7684a6fefa421a8e5
FantasticMao <maomao8017@gmail.com>
2019-02-02 03:06:57 +0800

    添加 Netty 示例代码

--- src/main/java/priv/mm/netty/Server.java
+++ src/main/java/priv/mm/netty/Server.java
@@ -1,4 +1,4 @@
-package priv.mm.netty.discard;
+package priv.mm.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
@@ -8,18 +8,20 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import priv.mm.netty.time.TimeServerHandler;
+
+import java.net.InetSocketAddress;
 
 /**
- * DiscardServer
- * Test Command: <code>telnet localhost 9999</code>
+ * Server
  *
  * @author maodh
- * @since 2018/6/24
+ * @since 2019/2/2
  */
-public class DiscardServer {
+public class Server {
     private int port;
 
-    public DiscardServer(int port) {
+    public Server(int port) {
         this.port = port;
     }
 
@@ -30,15 +32,19 @@ public class DiscardServer {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new DiscardServerHandler());
+                            // ch.pipeline().addLast(new DiscardServerHandler());
+                            ch.pipeline().addLast(new TimeServerHandler());
+                            // ch.pipeline().addLast(new HttpServerHandler());
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
-            ChannelFuture f = b.bind(port).sync();
+
+            ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } finally {
             workerGroup.shutdownGracefully();
@@ -47,6 +53,6 @@ public class DiscardServer {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        new DiscardServer(9999).run();
+        new Server(9999).run();
     }
 }
