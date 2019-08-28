8d8a9ee3a587e4261756c9bd81c1a7f5d532c42c
FantasticMao <maomao8017@gmail.com>
2019-08-28 15:41:38 +0800

    添加 Javassist 示例代码

---
+++ src/main/java/priv/mm/javassist/TestClass.java
@@ -0,0 +1,15 @@
+package priv.mm.javassist;
+
+/**
+ * TestClass
+ *
+ * @author maomao
+ * @since 2019-08-28
+ */
+public class TestClass {
+
+    @Override
+    public String toString() {
+        return "TestClass";
+    }
+}
