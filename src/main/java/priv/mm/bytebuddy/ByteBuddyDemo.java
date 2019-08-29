e5d50f4d92ee3849a48b703fd132fb86ffe8ede6
FantasticMao <maomao8017@gmail.com>
2019-08-29 16:01:28 +0800

    更新 Byte Buddy 示例代码

--- src/main/java/priv/mm/bytebuddy/ByteBuddyDemo.java
+++ src/main/java/priv/mm/bytebuddy/ByteBuddyDemo.java
@@ -2,7 +2,19 @@ package priv.mm.bytebuddy;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperCall;
 import net.bytebuddy.matcher.ElementMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.Callable;
 
 /**
  * ByteBuddyDemo
@@ -13,8 +25,9 @@ import net.bytebuddy.matcher.ElementMatchers;
  */
 public class ByteBuddyDemo {
 
-    public static void main(String[] args) throws Exception {
-        String toString = new ByteBuddy()
+    @Test
+    public void test() throws Exception {
+        Object obj = new ByteBuddy()
                 .subclass(Object.class)
                 .method(ElementMatchers.named("toString"))
                 .intercept(FixedValue.value("Hello Byte Buddy"))
@@ -23,6 +36,33 @@ public class ByteBuddyDemo {
                 .getLoaded()
                 .newInstance()
                 .toString();
-        System.out.println(toString);
+        Assert.assertEquals("Hello Byte Buddy", obj.toString());
+    }
+
+    @Test
+    public void methodDelegation() throws Exception {
+        Object obj = new ByteBuddy()
+                .subclass(Object.class)
+                .method(ElementMatchers.any())
+                .intercept(MethodDelegation.to(PrintInfoInterceptor.class))
+                .make()
+                .load(ClassLoader.getSystemClassLoader())
+                .getLoaded()
+                .newInstance();
+        obj.toString();
+    }
+
+    public static class PrintInfoInterceptor {
+
+        @RuntimeType
+        public static Object intercept(@SuperCall Callable<?> callable, @Origin Class<?> clazz, @Origin Method method,
+                                       @AllArguments Object[] arguments) throws Exception {
+            try {
+                return callable.call();
+            } finally {
+                System.out.printf("invoke %s#%s(%s)%n", clazz.getName(), method.getName(),
+                        String.join(",", Arrays.stream(arguments).map(Objects::toString).toArray(String[]::new)));
+            }
+        }
     }
 }
