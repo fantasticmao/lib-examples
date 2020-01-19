711a3c7bf9af51c5a48a55104df565fb97993401
FantasticMao <maomao8017@gmail.com>
2020-01-20 03:09:35 +0800

    添加 Go 基本语法练习代码

---
+++ demo-golang/src/hello/hello_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got '%q' want '%q'", got, want)
+        }
+    }
+
+    t.Run("saying Hello to people", func(t *testing.T) {
+        got := Hello("Chris", "")
+        want := "Hello, Chris"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("empty string default to 'world'", func(t *testing.T) {
+        got := Hello("", "")
+        want := "Hello, World"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Chinese", func(t *testing.T) {
+        got := Hello("世界", "Chinese")
+        want := "你好, 世界"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("in Japanese", func(t *testing.T) {
+        got := Hello("世界", "Japanese")
+        want := "こんにちは世界"
+        assertCorrectMessage(t, got, want)
+    })
+}
