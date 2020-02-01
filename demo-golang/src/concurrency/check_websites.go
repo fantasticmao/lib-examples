60c6b7015783e119ecb02f9bcb29dde58b2b0502
FantasticMao <maomao8017@gmail.com>
2020-02-02 03:41:55 +0800

    添加 Go 基本语法练习代码

--- demo-golang/src/concurrency/check_websites.go
+++ demo-golang/src/concurrency/check_websites.go
@@ -11,15 +11,12 @@ func CheckWebsites(checker WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	resultChannel := make(chan result)
 	for _, url := range urls {
-		// 开启一个新的 goroutine
-		go func(u string) {
-			// send statement
-			resultChannel <- result{u, checker(u)}
-		}(url)
+		go func(u string) { // 使用 func(){}() 匿名函数，开启一个新的 goroutine
+			resultChannel <- result{u, checker(u)} // send statement
+		}(url) // 设置匿名函数参数
 	}
 	for i := 0; i < len(urls); i++ {
-		// receive expression
-		result := <-resultChannel
+		result := <-resultChannel // receive expression
 		results[result.string] = result.bool
 	}
 	return results
---
+++ demo-golang/src/select/racer_test.go
@@ -0,0 +1,47 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		t.Helper()
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
+		got, _ := Racer(slowUrl, fastUrl)
+		want := fastUrl
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		t.Helper()
+		server := makeDelayedServer(25 * time.Millisecond)
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+}
+
+// 使用 httptest 模拟 http 服务
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
