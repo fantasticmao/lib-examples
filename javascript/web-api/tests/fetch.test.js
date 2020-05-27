8ff67fcf2e74b049e2dfc3b8807379556bd3097b
fantasticmao <maomao8017@gmail.com>
2020-05-27 16:37:38 +0800

    添加 JavaScript Web APIs 模块

---
+++ javascript/web-api/tests/fetch.test.js
@@ -0,0 +1,37 @@
+/**
+ * Fetch API - Web APIs | MDN https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+ * Using Fetch - Web APIs | MDN https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * WindowOrWorkerGlobalScope.fetch() - Web APIs | MDN https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+ */
+
+import fetch from 'node-fetch';
+import fetchMock from 'fetch-mock';
+
+test('fetch get', (done) => {
+  fetchMock.mock({
+    name: 'http://example.com',
+    method: 'GET',
+  }, {
+    status: 200,
+    body: 'OK',
+  });
+  fetch('http://example.com')
+      .then((response) => expect(response.ok).toBeTruthy())
+      .then(done)
+      .catch((error) => done.fail(error));
+});
+
+test('fetch post', (done) => {
+  fetchMock.mock({
+    name: 'http://example.com',
+    method: 'POST',
+  }, {
+    status: 200,
+    body: 'OK',
+  });
+  fetch('http://example.com', {
+    method: 'POST',
+  }).then((response) => expect(response.ok).toBeTruthy())
+      .then(done)
+      .catch((error) => done.fail(error));
+});
