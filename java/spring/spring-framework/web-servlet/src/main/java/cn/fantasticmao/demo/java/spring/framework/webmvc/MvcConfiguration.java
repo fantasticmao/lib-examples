49b684a34006879d34abe0c11ecd5411e6950d9a
fantasticmao <maomao8017@gmail.com>
2020-06-29 19:51:34 +0800

    添加 spring-security 模块

--- java/spring/spring-framework/web-servlet/src/main/java/cn/fantasticmao/demo/java/spring/framework/webmvc/MvcConfiguration.java
+++ java/spring/spring-framework/web-servlet/src/main/java/cn/fantasticmao/demo/java/spring/framework/webmvc/MvcConfiguration.java
@@ -5,29 +5,58 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.*;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 /**
- * ApplicationConfiguration
+ * MvcConfiguration
  *
  * @author maomao
- * @since 2020-06-28
+ * @since 2020-06-29
  */
+@EnableWebMvc
 @ComponentScan
 @Configuration
-public class ApplicationConfiguration implements WebApplicationInitializer {
+public class MvcConfiguration implements WebApplicationInitializer, WebMvcConfigurer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(ApplicationConfiguration.class);
+        applicationContext.register(MvcConfiguration.class);
 
         DispatcherServlet servlet = new DispatcherServlet(applicationContext);
         ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", servlet);
         registration.setLoadOnStartup(1);
         registration.addMapping("/");
+    }
+
+    /**
+     * 处理静态资源
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/public/");
+    }
+
+    /**
+     * 处理跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // test curl command:
+        // curl --request GET \
+        //  --url http://localhost:8080/hello \
+        //  --header 'origin: https://fantasticmao.cn'
+        registry.addMapping("/hello")
+            .allowedOrigins("https://fantasticmao.cn");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
     }
 }
