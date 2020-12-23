2e8111390757614db0970c15bf3652e5c4d20081
fantasticmao <maomao8017@gmail.com>
2020-12-23 20:12:07 +0800

    更新 斐波那契数列

--- java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/FibonacciNumber.java
+++ java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/FibonacciNumber.java
@@ -1,8 +1,6 @@
 package cn.fantasticmao.demo.java.algorithm;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * FibonacciNumber
@@ -11,20 +9,19 @@ import java.util.Map;
  * @since 2020-10-28
  */
 public class FibonacciNumber {
-    private static final Map<Integer, BigInteger> CACHE = new HashMap<>();
 
     public static BigInteger fin(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException();
-        }
         if (n <= 1) {
             return BigInteger.valueOf(n);
         }
-        if (CACHE.containsKey(n)) {
-            return CACHE.get(n);
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger sum = BigInteger.valueOf(0);
+        for (int i = 2; i <= n; i++) {
+            sum = a.add(b);
+            a = b;
+            b = sum;
         }
-        final BigInteger result = fin(n - 1).add(fin(n - 2));
-        CACHE.put(n, result);
-        return result;
+        return sum;
     }
 }
