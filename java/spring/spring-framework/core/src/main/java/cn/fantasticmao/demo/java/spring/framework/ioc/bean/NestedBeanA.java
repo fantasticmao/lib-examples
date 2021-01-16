f82037f085b104389389d719cf6542abba76a21c
fantasticmao <maomao8017@gmail.com>
2021-01-16 16:27:38 +0800

    更新 Spring Framework 模块

---
+++ java/spring/spring-framework/core/src/main/java/cn/fantasticmao/demo/java/spring/framework/ioc/bean/NestedBeanA.java
@@ -0,0 +1,28 @@
+package cn.fantasticmao.demo.java.spring.framework.ioc.bean;
+
+/**
+ * NestedBeanA
+ *
+ * @author maomao
+ * @since 2021-01-12
+ */
+public class NestedBeanA {
+    private NestedBeanB b;
+
+    public NestedBeanA() {
+    }
+
+    public NestedBeanA(NestedBeanB b) {
+        this.b = b;
+    }
+
+    // getter and setter
+
+    public NestedBeanB getB() {
+        return b;
+    }
+
+    public void setB(NestedBeanB b) {
+        this.b = b;
+    }
+}
