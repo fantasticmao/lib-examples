e6cd224f3ff36e67d36397c873652ee7aa9aa288
fantasticmao <maomao8017@gmail.com>
2021-01-27 14:24:53 +0800

    更新 三个线程依次输出 ABC

--- java/lang/src/main/java/cn/fantasticmao/demo/java/lang/concurrent/simulation/TurnPrint3.java
+++ java/lang/src/main/java/cn/fantasticmao/demo/java/lang/concurrent/simulation/TurnPrint3.java
@@ -1,7 +1,5 @@
 package cn.fantasticmao.demo.java.lang.concurrent.simulation;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -14,33 +12,31 @@ public class TurnPrint3 {
 
     private static class Task implements Runnable {
         private final char c;
+        private final int remainder;
         private final AtomicInteger count;
 
-        public Task(char c, AtomicInteger count) {
+        public Task(char c, int remainder, AtomicInteger count) {
             this.c = c;
+            this.remainder = remainder;
             this.count = count;
         }
 
         @Override
         public void run() {
-            System.out.printf("%c", c);
-            if (count.getAndIncrement() % 3 == 0) {
-                System.out.println();
+            while (!Thread.currentThread().isInterrupted()
+                && count.get() < 300) {
+                if (count.get() % 3 == remainder) {
+                    System.out.printf("%c%n", c);
+                    count.incrementAndGet();
+                }
             }
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        AtomicInteger count = new AtomicInteger(1);
-        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(30);
-        for (int i = 0; i < 10; i++) {
-            queue.put(new Task('X', count));
-            queue.put(new Task('Y', count));
-            queue.put(new Task('Z', count));
-        }
-        while (!Thread.currentThread().isInterrupted() && !queue.isEmpty()) {
-            Runnable task = queue.take();
-            task.run();
-        }
+    public static void main(String[] args) throws Exception {
+        AtomicInteger count = new AtomicInteger(0);
+        new Thread(new Task('A', 0, count), "Task-A").start();
+        new Thread(new Task('B', 1, count), "Task-B").start();
+        new Thread(new Task('C', 2, count), "Task-C").start();
     }
 }
