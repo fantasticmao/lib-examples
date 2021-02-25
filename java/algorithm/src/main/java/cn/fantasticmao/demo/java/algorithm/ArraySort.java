239a2830d8b9d5289bcb826dad068cd2733fb1d7
fantasticmao <maomao8017@gmail.com>
2021-02-25 18:03:19 +0800

    更新 排序算法

--- java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/ArraySort.java
+++ java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/ArraySort.java
@@ -4,13 +4,14 @@ package cn.fantasticmao.demo.java.algorithm;
  * ArraySort
  *
  * <pre>
- * +----------+----------------+----------+----------+------------+--------+
- * | 算法名称 | 平均时间复杂度 | 最好情况 | 最差情况 | 空间复杂度 | 稳定性 |
- * +----------+----------------+----------+----------+------------+--------+
- * | 冒泡排序 | O(n^2)         | O(n)     | O(n^2)   | O(1)       | 稳定   |
- * | 选择排序 | O(n^2)         | O(n^2)   | O(n^2)   | O(1)       | 不稳定 |
- * | 插入排序 | O(n^2)         | O(n)     | O(n^2)   | O(1)       | 稳定   |
- * +----------+----------------+----------+----------+------------+--------+
+ * +----------+----------------+-------------+-------------+------------+--------+
+ * | 算法名称 | 平均时间复杂度 |  最好情况   |  最差情况   | 空间复杂度 | 稳定性 |
+ * +----------+----------------+-------------+-------------+------------+--------+
+ * | 冒泡排序 | O(n^2)         | O(n)        | O(n^2)      | O(n)       | 稳定   |
+ * | 选择排序 | O(n^2)         | O(n^2)      | O(n^2)      | O(n)       | 不稳定 |
+ * | 插入排序 | O(n^2)         | O(n)        | O(n^2)      | O(n)       | 稳定   |
+ * | 归并排序 | O(n*log(n))    | O(n*log(n)) | O(n*log(n)) | O(n)       | 稳定   |
+ * +----------+----------------+-------------+-------------+------------+--------+
  * </pre>
  *
  * @author maomao
@@ -18,30 +19,29 @@ package cn.fantasticmao.demo.java.algorithm;
  */
 public interface ArraySort {
 
-    int[] sort(int[] array);
+    int[] sortArray(int[] nums);
 
     /**
      * 冒泡排序
      *
      * @see <a href="https://en.wikipedia.org/wiki/Bubble_sort">维基百科</a>
-     * @see <a href="https://www.runoob.com/w3cnote/bubble-sort.html">菜鸟教程</a>
      */
     class BubbleSort implements ArraySort {
 
         @Override
-        public int[] sort(int[] array) {
-            // 需要经过 array.length - 1 次比较
-            for (int i = 0; i < array.length - 1; i++) {
+        public int[] sortArray(int[] nums) {
+            // 需要经过 nums.length - 1 次比较
+            for (int i = 0; i < nums.length - 1; i++) {
                 // 标记当前循环中是否发生过交换
                 boolean changedFlag = false;
 
-                // 每次需要经过 array.length - 1 - i 次比较
-                for (int j = 0; j + 1 < array.length - i; j++) {
-                    if (array[j] > array[j + 1]) {
+                // 每次需要经过 nums.length - 1 - i 次比较
+                for (int j = 0; j + 1 < nums.length - i; j++) {
+                    if (nums[j] > nums[j + 1]) {
                         // 交换两数
-                        int temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
 
                         changedFlag = true;
                     }
@@ -52,7 +52,7 @@ public interface ArraySort {
                     break;
                 }
             }
-            return array;
+            return nums;
         }
     }
 
@@ -60,18 +60,17 @@ public interface ArraySort {
      * 选择排序
      *
      * @see <a href="https://en.wikipedia.org/wiki/Selection_sort">维基百科</a>
-     * @see <a href="https://www.runoob.com/w3cnote/selection-sort.html">菜鸟教程</a>
      */
     class SelectSort implements ArraySort {
 
         @Override
-        public int[] sort(int[] array) {
-            // 需要经过 array.length - 1 次查找
-            for (int i = 0; i < array.length - 1; i++) {
+        public int[] sortArray(int[] nums) {
+            // 需要经过 nums.length - 1 次查找
+            for (int i = 0; i < nums.length - 1; i++) {
                 int minIndex = i;
-                // 每次需要 array.length - 1 - i 次查找
-                for (int j = i + 1; j < array.length; j++) {
-                    if (array[j] < array[minIndex]) {
+                // 每次需要 nums.length - 1 - i 次查找
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] < nums[minIndex]) {
                         // 查找最小数
                         minIndex = j;
                     }
@@ -79,12 +78,12 @@ public interface ArraySort {
 
                 if (minIndex != i) {
                     // 交换两数
-                    int temp = array[i];
-                    array[i] = array[minIndex];
-                    array[minIndex] = temp;
+                    int temp = nums[i];
+                    nums[i] = nums[minIndex];
+                    nums[minIndex] = temp;
                 }
             }
-            return array;
+            return nums;
         }
     }
 
@@ -92,29 +91,104 @@ public interface ArraySort {
      * 插入排序
      *
      * @see <a href="https://en.wikipedia.org/wiki/Insertion_sort">维基百科</a>
-     * @see <a href="https://www.runoob.com/w3cnote/insertion-sort.html">菜鸟教程</a>
      */
     class InsertSort implements ArraySort {
 
         @Override
-        public int[] sort(int[] array) {
-            // 需要经过 array.length - 1 次查找
-            for (int i = 1; i < array.length; i++) {
+        public int[] sortArray(int[] nums) {
+            // 需要经过 nums.length - 1 次查找
+            for (int i = 1; i < nums.length; i++) {
                 // 暂存待插入的元素
-                int currentValue = array[i];
+                int currentValue = nums[i];
                 // 确定待插入的元素下标
                 int insertIndex = i;
-                for (; insertIndex > 0 && array[insertIndex - 1] > currentValue; insertIndex--) {
-                    array[insertIndex] = array[insertIndex - 1];
+                for (; insertIndex > 0 && nums[insertIndex - 1] > currentValue; insertIndex--) {
+                    nums[insertIndex] = nums[insertIndex - 1];
                 }
 
                 if (insertIndex != i) {
                     // 插入元素
-                    array[insertIndex] = currentValue;
+                    nums[insertIndex] = currentValue;
                 }
             }
-            return array;
+            return nums;
         }
     }
 
+    /**
+     * 归并排序
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Merge_sort">维基百科</a>
+     */
+    class MergeSort implements ArraySort {
+
+        @Override
+        public int[] sortArray(int[] nums) {
+            mergeSort(nums, 0, nums.length - 1);
+            return nums;
+        }
+
+        private void mergeSort(int[] nums, int start, int end) {
+            if (start < end) {
+                int mid = start + (end - start) / 2;
+                // 分解为子问题，递归求解子问题
+                mergeSort(nums, start, mid);
+                mergeSort(nums, mid + 1, end);
+                // 合并子问题的解
+                merge(nums, start, mid, end);
+            }
+        }
+
+        /**
+         * 归并操作：合并两个数组
+         */
+        private void merge(int[] nums, int start, int mid, int end) {
+            int[] result = new int[end - start + 1];
+            int i = 0, i1 = start, i2 = mid + 1;
+            for (; i1 <= mid && i2 <= end; i++) {
+                if (nums[i1] < nums[i2]) {
+                    result[i] = nums[i1];
+                    i1++;
+                } else {
+                    result[i] = nums[i2];
+                    i2++;
+                }
+            }
+            for (; i1 <= mid; i++, i1++) {
+                result[i] = nums[i1];
+            }
+            for (; i2 <= end; i++, i2++) {
+                result[i] = nums[i2];
+            }
+            System.arraycopy(result, 0, nums, start, result.length);
+        }
+    }
+
+    /**
+     * 堆排序
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Heapsort">维基百科</a>
+     */
+    class HeapSort implements ArraySort {
+
+        @Override
+        public int[] sortArray(int[] nums) {
+            // TODO
+            return nums;
+        }
+    }
+
+    /**
+     * 快速排序
+     *
+     * @see <a href="https://en.wikipedia.org/wiki/Quicksort">维基百科</a>
+     */
+    class QuickSort implements ArraySort {
+
+        @Override
+        public int[] sortArray(int[] nums) {
+            // TODO
+            return nums;
+        }
+    }
 }
--- java/algorithm/src/test/java/cn/fantasticmao/demo/java/algorithm/ArraySortTest.java
+++ java/algorithm/src/test/java/cn/fantasticmao/demo/java/algorithm/ArraySortTest.java
@@ -1,6 +1,7 @@
 package cn.fantasticmao.demo.java.algorithm;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -13,24 +14,44 @@ import java.util.Arrays;
  */
 public class ArraySortTest {
 
-    private int[] array = new int[]{91, 67, 14, 83, 23, 30, 20, 59, 98, 86};
-    private int[] expected = new int[]{14, 20, 23, 30, 59, 67, 83, 86, 91, 98};
+    private final int[] array = new int[]{91, 67, 14, 83, 23, 30, 20, 59, 98, 86};
+    private final int[] expected = new int[]{14, 20, 23, 30, 59, 67, 83, 86, 91, 98};
 
     @Test
     public void bubbleSort() {
-        int[] actual = new ArraySort.BubbleSort().sort(Arrays.copyOf(array, array.length));
+        int[] actual = new ArraySort.BubbleSort().sortArray(Arrays.copyOf(array, array.length));
         Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void selectSort() {
-        int[] actual = new ArraySort.SelectSort().sort(Arrays.copyOf(array, array.length));
+        int[] actual = new ArraySort.SelectSort().sortArray(Arrays.copyOf(array, array.length));
         Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
     public void insertSort() {
-        int[] actual = new ArraySort.InsertSort().sort(Arrays.copyOf(array, array.length));
+        int[] actual = new ArraySort.InsertSort().sortArray(Arrays.copyOf(array, array.length));
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void mergeSort() {
+        int[] actual = new ArraySort.MergeSort().sortArray(Arrays.copyOf(array, array.length));
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Ignore
+    @Test
+    public void heapSort() {
+        int[] actual = new ArraySort.HeapSort().sortArray(Arrays.copyOf(array, array.length));
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Ignore
+    @Test
+    public void quickSort() {
+        int[] actual = new ArraySort.QuickSort().sortArray(Arrays.copyOf(array, array.length));
         Assert.assertArrayEquals(expected, actual);
     }
 
