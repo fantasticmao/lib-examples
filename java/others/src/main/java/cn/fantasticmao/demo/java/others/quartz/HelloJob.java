0aef2b3f7a0b7e77df5433a8d80ebfc18831ef35
fantasticmao <maomao8017@gmail.com>
2021-04-07 10:26:41 +0800

    添加 Quartz 示例代码

---
+++ java/others/src/main/java/cn/fantasticmao/demo/java/others/quartz/HelloJob.java
@@ -0,0 +1,19 @@
+package cn.fantasticmao.demo.java.others.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * HelloJob
+ *
+ * @author maomao
+ * @since 2021-04-07
+ */
+public class HelloJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        System.out.printf("%s - Hello Quartz!", Thread.currentThread().getName());
+    }
+}
