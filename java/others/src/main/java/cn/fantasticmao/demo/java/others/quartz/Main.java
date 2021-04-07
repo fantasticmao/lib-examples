b3665b6a9cbfda0103ce8b2212feac9c539e6796
fantasticmao <maomao8017@gmail.com>
2021-04-07 10:26:41 +0800

    添加 Quartz 示例代码

---
+++ java/others/src/main/java/cn/fantasticmao/demo/java/others/quartz/Main.java
@@ -0,0 +1,33 @@
+package cn.fantasticmao.demo.java.others.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Main
+ *
+ * @author maomao
+ * @since 2021-04-07
+ */
+public class Main {
+
+    public static void main(String[] args) throws SchedulerException, InterruptedException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+        scheduler.start();
+
+        JobDetail job = JobBuilder.newJob(HelloJob.class)
+            .withIdentity("helloJob", "group1")
+            .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+            .withIdentity("helloTrigger", "group1")
+            .build();
+        scheduler.scheduleJob(job, trigger);
+
+        TimeUnit.SECONDS.sleep(3);
+        scheduler.shutdown();
+    }
+}
