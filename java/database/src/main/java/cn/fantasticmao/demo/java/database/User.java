b31d38ce1dda47377ea643e5b18cfbbe55030026
fantasticmao <maomao8017@gmail.com>
2021-12-23 00:35:13 +0800

    添加 HBase 示例代码

--- java/database/src/main/java/cn/fantasticmao/demo/java/database/User.java
+++ java/database/src/main/java/cn/fantasticmao/demo/java/database/User.java
@@ -1,21 +1,32 @@
 package cn.fantasticmao.demo.java.database;
 
+import cn.fantasticmao.demo.java.database.hbase.HbaseRepository;
+
 /**
  * User
  *
  * @author fantasticmao
+ * @see cn.fantasticmao.demo.java.database.h2.H2Repository
+ * @see HbaseRepository
+ * @see cn.fantasticmao.demo.java.database.sqlite.SqliteRepository
  * @since 2021-12-18
  */
 public class User {
     private Integer id;
     private String name;
+    private String email;
 
     public User() {
     }
 
     public User(Integer id, String name) {
+        this(id, name, null);
+    }
+
+    public User(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
     }
 
     // getter and setter
@@ -35,4 +46,13 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
