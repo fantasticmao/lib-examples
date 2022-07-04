70fad23faf61b2e996e4af330b5629d344fb1fcd
fantasticmao <maomao8017@gmail.com>
2022-07-05 01:10:08 +0800

    添加 LeetCode: Reverse Linked List

---
+++ java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/leetcode/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+package cn.fantasticmao.demo.java.algorithm.leetcode;
+
+import cn.fantasticmao.demo.java.algorithm.leetcode.model.ListNode;
+
+/**
+ * ReverseLinkedList
+ *
+ * @author fantasticmao
+ * @see <a href="https://leetcode.com/problems/reverse-linked-list/">Reverse Linked List</a>
+ * @since 2022-07-05
+ */
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            // 保存当前节点的 next 节点
+            ListNode next = current.next;
+            // 将当前节点的 next 指向 prev 节点
+            current.next = prev;
+
+            // 驱动下一次迭代
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
---
+++ java/algorithm/src/main/java/cn/fantasticmao/demo/java/algorithm/leetcode/model/ListNode.java
@@ -0,0 +1,24 @@
+package cn.fantasticmao.demo.java.algorithm.leetcode.model;
+
+/**
+ * ListNode
+ *
+ * @author fantasticmao
+ * @since 2022-07-05
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
