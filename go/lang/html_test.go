945c12a2635e29b1ac366f1bbb6a0843dbd0b052
fantasticmao <maomao8017@gmail.com>
2023-03-13 11:45:58 +0800

    更新 Go standard library 示例代码

--- go/lang/html_test.go
+++ go/lang/html_test.go
@@ -6,12 +6,14 @@ import (
 	"testing"
 )
 
-func TestEscape(t *testing.T) {
+// see https://pkg.go.dev/html
+
+func TestHtmlEscape(t *testing.T) {
 	s := html.EscapeString("<script>eval(\"1+2\")</script>")
 	assert.Equal(t, "&lt;script&gt;eval(&#34;1+2&#34;)&lt;/script&gt;", s)
 }
 
-func TestUnescape(t *testing.T) {
+func TestHtmlUnescape(t *testing.T) {
 	s := html.UnescapeString("&lt;script&gt;eval(&#34;1+2&#34;)&lt;/script&gt;")
 	assert.Equal(t, "<script>eval(\"1+2\")</script>", s)
 }
