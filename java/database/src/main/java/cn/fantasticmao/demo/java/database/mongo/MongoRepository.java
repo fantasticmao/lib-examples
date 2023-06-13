f4c46c13a242bc0833f48f981e29e64955ba68b5
fantasticmao <maomao8017@gmail.com>
2023-06-13 21:06:26 +0800

    更新 MongoDB 示例代码

---
+++ java/database/src/main/java/cn/fantasticmao/demo/java/database/mongo/MongoRepository.java
@@ -0,0 +1,83 @@
+package cn.fantasticmao.demo.java.database.mongo;
+
+import cn.fantasticmao.demo.java.database.User;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.io.Closeable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * MongoRepository
+ * <p>
+ * 启动 MongoDB Docker 容器
+ *
+ * @author fantasticmao
+ * @see <a href="https://www.mongodb.com/docs/manual/tutorial/getting-started/">Getting Started - MongoDB Manual</a>
+ * @see <a href="https://www.mongodb.com/docs/drivers/java/sync/current/quick-start/">Quick Start - MongoDB Drivers Java Sync</a>
+ * @since 2019-05-22
+ */
+public class MongoRepository implements Closeable {
+    private final MongoClient mongoClient;
+    private final MongoCollection<Document> collection;
+
+    public MongoRepository(String uri) {
+        this.mongoClient = MongoClients.create(uri);
+        this.collection = this.mongoClient
+            .getDatabase("t_users")
+            .getCollection("users");
+    }
+
+    public Document queryById(int id) {
+        return this.collection.find(Filters.eq("id", id)).first();
+    }
+
+    public FindIterable<Document> queryByIdIn(List<Integer> ids) {
+        return this.collection.find(Filters.in("id", ids))
+            .sort(Sorts.descending("id"));
+    }
+
+    public Document queryByName(String name) {
+        return this.collection.find(Filters.eq("name", name)).first();
+    }
+
+    public InsertManyResult insert(User... users) {
+        List<Document> documents = Arrays.stream(users)
+            .map(user -> new Document()
+                .append("id", user.getId())
+                .append("name", user.getName())
+                .append("email", user.getEmail())
+            )
+            .collect(Collectors.toList());
+        return this.collection.insertMany(documents);
+    }
+
+    public UpdateResult update(User user) {
+        Bson updates = Updates.combine(
+            Updates.set("name", user.getName()),
+            Updates.set("email", user.getEmail())
+        );
+        return this.collection.updateMany(Filters.eq("id", user.getId()), updates);
+    }
+
+    public DeleteResult deleteById(Integer id) {
+        return this.collection.deleteMany(Filters.eq("id", id));
+    }
+
+    @Override
+    public void close() {
+        mongoClient.close();
+    }
+}
---
+++ java/database/src/test/java/cn/fantasticmao/demo/java/database/mongo/MongoRepositoryTest.java
@@ -0,0 +1,60 @@
+package cn.fantasticmao.demo.java.database.mongo;
+
+import cn.fantasticmao.demo.java.database.User;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.InsertManyResult;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.Document;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * MongoRepositoryTest
+ *
+ * @author maodaohe
+ * @since 2023-06-13
+ */
+public class MongoRepositoryTest {
+
+    @Test
+    public void quickStart() {
+        String uri = "mongodb://localhost:27017";
+        try (MongoRepository repository = new MongoRepository(uri)) {
+            InsertManyResult insertResult = repository.insert(
+                new User(1, "Tom", "tom@google.com"),
+                new User(2, "Bob", "bob@apple.com"),
+                new User(3, "Anni", "anni@google.com")
+            );
+            Assert.assertTrue(insertResult.wasAcknowledged());
+            Assert.assertEquals(3, insertResult.getInsertedIds().size());
+
+            UpdateResult updateResult = repository.update(new User(2, "Bob", "bob@google.com"));
+            Assert.assertTrue(updateResult.wasAcknowledged());
+            Assert.assertTrue(updateResult.getModifiedCount() > 0);
+
+            Document document = repository.queryById(2);
+            Assert.assertEquals("bob@google.com", document.getString("email"));
+            document = repository.queryByName("Bob");
+            Assert.assertEquals(2, document.getInteger("id").intValue());
+            Assert.assertEquals("bob@google.com", document.getString("email"));
+
+            FindIterable<Document> documents = repository.queryByIdIn(Arrays.asList(1, 2, 3));
+            for (Document doc : documents) {
+                System.out.println(doc.toJson());
+            }
+
+            DeleteResult deleteResult = repository.deleteById(1);
+            Assert.assertTrue(deleteResult.wasAcknowledged());
+            Assert.assertTrue(deleteResult.getDeletedCount() > 0);
+            deleteResult = repository.deleteById(2);
+            Assert.assertTrue(deleteResult.wasAcknowledged());
+            Assert.assertTrue(deleteResult.getDeletedCount() > 0);
+            deleteResult = repository.deleteById(3);
+            Assert.assertTrue(deleteResult.wasAcknowledged());
+            Assert.assertTrue(deleteResult.getDeletedCount() > 0);
+        }
+    }
+}
