b30a0177c3a8ef875d759b8f2234e2454cef6b8a
fantasticmao <maomao8017@gmail.com>
2023-06-14 16:16:30 +0800

    更新 docker-compose.yml

--- java/database/src/main/java/cn/fantasticmao/demo/java/database/kafka/MessageProducer.java
+++ java/database/src/main/java/cn/fantasticmao/demo/java/database/kafka/MessageProducer.java
@@ -7,7 +7,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,7 +20,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class MessageProducer {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         Map<String, Object> configs = new HashMap<>();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstant.BOOTSTRAP_SERVERS);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
