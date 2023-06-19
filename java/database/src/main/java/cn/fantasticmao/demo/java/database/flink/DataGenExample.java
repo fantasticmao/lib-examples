8933d4344b52ec78161f5715c57bbddf4e314fcd
fantasticmao <maomao8017@gmail.com>
2023-06-20 01:21:03 +0800

    更新 Flink 示例代码

--- java/database/src/main/java/cn/fantasticmao/demo/java/database/flink/DataGenExample.java
+++ java/database/src/main/java/cn/fantasticmao/demo/java/database/flink/DataGenExample.java
@@ -4,32 +4,35 @@ import cn.fantasticmao.demo.java.database.User;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * FlinkDataStreamExample
+ * DataGenExample
  *
  * @author fantasticmao
+ * @see <a href="https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/connectors/datastream/datagen/">DataGen Connector</a>
  * @since 2023-06-19
  */
-public class FlinkDataStreamExample {
+public class DataGenExample {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.getConfig().setClosureCleanerLevel(ExecutionConfig.ClosureCleanerLevel.NONE);
 
-        DataStreamSource<User> users = env.fromElements(
-            new User(1, "Tom", 20),
-            new User(2, "Bob", 17),
-            new User(3, "Anni", 18)
-        );
-        DataStream<User> adults = users.filter(new FilterFunction<User>() {
-            @Override
-            public boolean filter(User user) throws Exception {
-                return user.getAge() >= 18;
-            }
-        });
+        DataStream<User> input = env.fromElements(
+                new User(1, "Tom", 20),
+                new User(2, "Bob", 17),
+                new User(3, "Anni", 18)
+            )
+            .name("Generator Source");
+
+        DataStream<User> adults = input.filter(new FilterFunction<User>() {
+                @Override
+                public boolean filter(User user) throws Exception {
+                    return user.getAge() >= 18;
+                }
+            })
+            .name("Filter out Adults");
 
         adults.print();
 
