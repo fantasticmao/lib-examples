86c9edb13b07692f0ed7f5c64db07ca850195c7e
fantasticmao <maomao8017@gmail.com>
2023-11-30 02:28:38 +0800

    更新 Spring Web MVC 模块

---
+++ java/spring/spring-framework/web-servlet/src/main/java/cn/fantasticmao/demo/java/spring/framework/webmvc/filter/LoggingFilter.java
@@ -0,0 +1,45 @@
+package cn.fantasticmao.demo.java.spring.framework.webmvc.filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.annotation.Nonnull;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * LoggingFilter
+ *
+ * @author fantasticmao
+ * @since 2023-11-29
+ */
+@WebFilter(filterName = "Logging filter", urlPatterns = "/*")
+public class LoggingFilter extends OncePerRequestFilter {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+
+    @Override
+    protected void doFilterInternal(@Nonnull HttpServletRequest request, @Nonnull HttpServletResponse response,
+                                    @Nonnull FilterChain filterChain) throws ServletException, IOException {
+        LocalDateTime start = LocalDateTime.now();
+        System.out.printf("Request time: %s\n", FORMATTER.format(start));
+        String url = request.getRequestURL().toString();
+        System.out.printf("Request path: %s\n", URI.create(url).getPath());
+
+        filterChain.doFilter(request, response);
+
+        LocalDateTime end = LocalDateTime.now();
+        System.out.printf("Response time: %s\n", FORMATTER.format(end));
+        System.out.printf("Response status: %d\n", response.getStatus());
+
+        Duration duration = Duration.between(start, end);
+        System.out.printf("Duration time: %dms\n", TimeUnit.NANOSECONDS.toMillis(duration.getNano()));
+    }
+}
