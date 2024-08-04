d25c9cdf0ce0c95bd9af111e3d535591ff9ebd04
fantasticmao <maomao8017@gmail.com>
2024-08-04 14:13:00 +0800

    添加 PostgreSQL 示例代码

---
+++ java/database/src/test/java/cn/fantasticmao/demo/java/database/postgresql/PostgresCrudRepositoryTest.java
@@ -0,0 +1,32 @@
+package cn.fantasticmao.demo.java.database.postgresql;
+
+import cn.fantasticmao.demo.java.database.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * PostgresCrudRepositoryTest
+ *
+ * @author fantasticmao
+ * @since 2024-08-04
+ */
+public class PostgresCrudRepositoryTest {
+
+    @Test
+    public void test() throws SQLException {
+        try (PostgresCrudRepository repository = new PostgresCrudRepository()) {
+            boolean insertStatus = repository.insert(User.Tom);
+            Assert.assertTrue(insertStatus);
+            insertStatus = repository.insert(User.Bob);
+            Assert.assertTrue(insertStatus);
+            insertStatus = repository.insert(User.Anni);
+            Assert.assertTrue(insertStatus);
+
+            List<User> userList = repository.selectAll();
+            Assert.assertEquals(3, userList.size());
+        }
+    }
+}
