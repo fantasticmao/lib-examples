e93cd6c8bc210b4c4cba26e05b607b7f09c53d68
fantasticmao <maomao8017@gmail.com>
2025-11-19 10:51:34 +0800

    更新 Python 示例代码

--- python/lang/tests/test_calculate.py
+++ python/lang/tests/test_calculate.py
@@ -13,8 +13,8 @@ def test_numbers():
     assert 2 == 17 % 3
 
     # ** 运算符计算幂
-    assert 25 == 5**2
-    assert 1024 == 2**10
+    assert 25 == 5 ** 2
+    assert 1024 == 2 ** 10
 
     width = 20
     height = 5 * 9
@@ -51,11 +51,80 @@ Usage: thingy [OPTIONS]
     )
 
     # 字符串可以用 + 合并（粘到一起），也可以用 * 重复
-    print(3 * "un" + "ium")
+    assert "unununium" == (3 * "un" + "ium")
 
     # 相邻的两个或多个字符串字面值，会自动合并
-    print("Py" "thon")
+    assert "Python" == "Py" "thon"
     print("Put several strings within parentheses " "to have them joined together.")
 
     prefix = "Py"
-    print(prefix + "thon")
+    assert "Python" == prefix + "thon"
+
+    # 字符串支持索引 （下标访问）
+    assert "P" == "Python"[0]
+    assert "n" == "Python"[5]
+    assert "n" == "Python"[-1]
+    assert "o" == "Python"[-2]
+    assert "Py" == "Python"[0:2]
+    assert "tho" == "Python"[2:5]
+    assert "Py" == "Python"[:2]
+    assert "on" == "Python"[4:]
+    assert "on" == "Python"[-2:]
+    assert "on" == "Python"[4:42]
+    assert "" == "Python"[42:]
+
+    # len() 返回字符串的长度
+    assert 6 == len("Python")
+
+
+def test_lists():
+    squares = [1, 4, 9, 16, 25]
+
+    # 列表也支持索引和切片
+    assert 1 == squares[0]
+    assert 25 == squares[-1]
+    assert [9, 16, 25] == squares[-3:]
+
+    # 列表支持合并操作
+    assert [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] == squares + [36, 49, 64, 81, 100]
+
+    #  列表是可变类型，其内容可以改变
+    cubes = [1, 8, 27, 65, 125]
+    cubes[3] = 64
+    assert [1, 8, 27, 64, 125] == cubes
+
+    cubes.append(216)  # 添加 6 的立方
+    cubes.append(7 ** 3)  # 和 7 的立方
+    assert [1, 8, 27, 64, 125, 216, 343] == cubes
+
+    # 将一个列表赋值给一个变量时，该变量将引用现有的列表。通过一个变量对列表所做的任何更改，都会被引用它的所有其他变量看到。
+    rgb = ["Red", "Green", "Blue"]
+    rgba = rgb
+    assert id(rgb) == id(rgba)
+
+    # 切片操作返回包含请求元素的新列表
+    correct_rgba = rgba[:]
+    correct_rgba[-1] = "Alpha"
+    assert ["Red", "Green", "Alpha"] == correct_rgba
+    assert ["Red", "Green", "Blue"] == rgba
+
+    # 为切片赋值可以改变列表大小，甚至清空整个列表
+    letters = ["a", "b", "c", "d", "e", "f", "g"]
+    letters[2:5] = ["C", "D", "E"]
+    assert ["a", "b", "C", "D", "E", "f", "g"] == letters
+    letters[2:5] = []
+    assert ["a", "b", "f", "g"] == letters
+    letters[:] = []
+    assert [] == letters
+
+    # # len() 返回列表的长度
+    letters = ["a", "b", "c", "d"]
+    assert 4 == len(letters)
+
+    # 列表支持嵌套
+    a = ["a", "b", "c"]
+    n = [1, 2, 3]
+    x = [a, n]
+    assert [["a", "b", "c"], [1, 2, 3]] == x
+    assert ["a", "b", "c"] == x[0]
+    assert "b" == x[0][1]
