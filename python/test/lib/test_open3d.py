954469a3b0a7e624ff72c6f89073ba82521a5920
fantasticmao <maomao8017@gmail.com>
2026-05-27 16:20:48 +0800

    添加 Python Open3D 示例代码

--- python/test/lib/test_open3d.py
+++ python/test/lib/test_open3d.py
@@ -1,7 +1,12 @@
 import open3d as o3d
 
 
-def test_quick_start():
-    mesh = o3d.geometry.TriangleMesh.create_sphere()
-    mesh.compute_vertex_normals()
-    o3d.visualization.draw(mesh, raw_mode=True)
+def test_point_cloud():
+    pcd = o3d.t.io.read_point_cloud("../939452553.pcd")
+    positions = pcd.point.positions.numpy()
+    print(positions.shape)
+    print(positions.dtype)
+
+    intensity = pcd.point["intensity"].numpy()
+    print(intensity.shape)
+    print(intensity.dtype)
