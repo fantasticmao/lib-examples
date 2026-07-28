9041810c9d80be94feac25686fa3cdc2041ca5e6
fantasticmao <maomao8017@gmail.com>
2026-07-28 14:33:12 +0800

    更新 Python Numpy 示例代码

--- python/test/lib/test_numpy.py
+++ python/test/lib/test_numpy.py
@@ -1,3 +1,5 @@
+# https://numpy.org/doc/stable/user/quickstart.html
+
 import math
 
 import numpy as np
@@ -5,18 +7,21 @@ import numpy as np
 
 def test_create():
     a = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
-    print(a.reshape(3, 3))
+    print(a)
     a = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
     print(a)
 
     b = np.zeros((4, 4))
-    print(b)
+    np.testing.assert_array_equal(b, [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+
     b = np.ones((4, 4), dtype=np.int16)
-    print(b)
+    np.testing.assert_array_equal(b, [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])
+
     b = np.arange(0, 10, 2)
-    print(b)
+    np.testing.assert_array_equal(b, [0, 2, 4, 6, 8])
+
     b = np.linspace(0, 1, 5)
-    print(b)
+    np.testing.assert_array_equal(b, [0, 0.25, 0.5, 0.75, 1])
 
 
 def test_basic_operate():
@@ -29,6 +34,9 @@ def test_basic_operate():
     # 矩阵数乘
     d = b * 2
     np.testing.assert_array_equal(d, [0, 2, 4, 6])
+    d = b ** 2
+    np.testing.assert_array_equal(d, [0, 1, 4, 9])
+    np.testing.assert_array_equal(d < 5, [True, True, True, False])
 
     a = np.array([[1, 1], [0, 1]])
     b = np.array([[2, 0], [3, 4]])
@@ -54,16 +62,20 @@ def test_basic_operate():
 
     a = np.arange(12).reshape(3, 4)
     np.testing.assert_array_equal(a, [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]])
+    # axis 指定要被聚合（消除）的维度，计算结束后，该维度会从 shape 中消失。
     np.testing.assert_array_equal(a.sum(axis=0), [12, 15, 18, 21])
     np.testing.assert_array_equal(a.sum(axis=1), [6, 22, 38])
 
 
 def test_function():
     a = np.arange(3)
+
     b = np.exp(a)
     np.testing.assert_allclose(b, [1, np.e, math.pow(np.e, 2)])
+
     c = np.sqrt(a)
     np.testing.assert_allclose(c, [0, 1, math.sqrt(2)])
+
     d = np.add(a, np.array([2, -1, 4]))
     np.testing.assert_equal(d, [2, 0, 6])
 
@@ -77,3 +89,37 @@ def test_slice():
     assert b[2, 3] == 23
     np.testing.assert_array_equal(b[:3, :3], [[0, 1, 2], [10, 11, 12], [20, 21, 22]])
     np.testing.assert_array_equal(b[:, 3], [3, 13, 23, 33])
+
+    c = [e for e in b]
+    assert np.array_equal(c, [[0, 1, 2, 3], [10, 11, 12, 13], [20, 21, 22, 23], [30, 31, 32, 33]])
+
+    c = [e for e in b.flat]
+    assert np.array_equal(c, [0, 1, 2, 3, 10, 11, 12, 13, 20, 21, 22, 23, 30, 31, 32, 33])
+
+
+def test():
+    a = np.array(
+        [
+            9.9519893946758906e-03,
+            -1.6900985313727641e-03,
+            -9.9994904943904162e-01,
+            5.4723829086017362e-02,
+            9.9994134732940854e-01,
+            -4.2565346745034871e-03,
+            9.9591070690140948e-03,
+            1.3667934866764725e-01,
+            -4.2731496739053347e-03,
+            -9.9998951268476766e-01,
+            1.6476384146416896e-03,
+            1.6583408747486148e00,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+        ],
+        dtype=np.float64,
+    ).reshape(4, 4)
+    print(a)
+    a_inv = np.linalg.inv(a)
+    print(a_inv)
+    print(np.around(a @ a_inv))
