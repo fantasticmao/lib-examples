46c4e5e4ae7ad105362fd73b6e2695643bbdbb5a
fantasticmao <maomao8017@gmail.com>
2026-08-07 15:26:09 +0800

    更新 Python Draco 示例代码

--- python/test/lib/test_open3d.py
+++ python/test/lib/test_open3d.py
@@ -1,8 +1,10 @@
 import open3d as o3d
 
+pcd_file = "../939452553.pcd"
 
-def test_point_cloud():
-    pcd = o3d.t.io.read_point_cloud("../939452553.pcd")
+
+def test_point_cloud_read():
+    pcd = o3d.t.io.read_point_cloud(pcd_file)
     positions = pcd.point.positions.numpy()
     print(positions.shape)
     print(positions.dtype)
@@ -10,3 +12,8 @@ def test_point_cloud():
     intensity = pcd.point["intensity"].numpy()
     print(intensity.shape)
     print(intensity.dtype)
+
+
+def test_point_cloud_visualization():
+    pcd = o3d.t.io.read_point_cloud(pcd_file)
+    o3d.visualization.draw_geometries(geometry_list=[pcd.to_legacy()], width=1024, height=768)
